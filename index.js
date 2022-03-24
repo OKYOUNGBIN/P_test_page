5032c9a6644a5507a59612bbc434d87d7e4ec88d
YOUNG BIN OK <redmangos7277@gmail.com>
2022-03-24 13:21:03 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const modelViewerVariants = document.querySelector("model-viewer#xr_box");
+const select = document.querySelector('#btnBox');
+
+modelViewerVariants.addEventListener('load', () => {
+    const names = modelViewerVariants.availableVariants;
+    for (const name of names) {
+        const button = document.createElement('BUTTON');
+        button.value = name;
+        button.textContent = name;
+        button.id = "btn"
+        select.appendChild(button);
+    }
+    console.log({modelViewerVariants})
+});
+
+select.addEventListener('click', (event) => {
+    modelViewerVariants.variantName = event.target.value === 'default' ? null : event.target.value;
+});
