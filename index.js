213be3ab2c4577855f92b454f2b6ab402ae59438
YOUNG BIN OK <redmangos7277@gmail.com>
2022-05-25 09:25:47 +0900

    test

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-//const modelViewerVariants = document.querySelector("model-viewer#xr_box");
-// const select = document.querySelector('#btnBox');
+const modelViewerVariants = document.querySelector("model-viewer#xr_box");
+const select = document.querySelector('#btnBox');
 
-// modelViewerVariants.addEventListener('load', () => {
-//     const names = modelViewerVariants.availableVariants;
-//     for (const name of names) {
-//         const button = document.createElement('BUTTON');
-//         button.value = name;
-//         button.textContent = name;
-//         button.id = "btn"
-//         select.appendChild(button);
-//     }
-//     console.log({modelViewerVariants})
-//     console.log({modelViewerVariants})
-// });
+modelViewerVariants.addEventListener('load', () => {
+    const names = modelViewerVariants.availableVariants;
+    for (const name of names) {
+        const button = document.createElement('BUTTON');
+        button.value = name;
+        button.textContent = name;
+        button.id = "btn"
+        select.appendChild(button);
+    }
+    console.log({modelViewerVariants})
+    console.log({modelViewerVariants})
+});
 
-// select.addEventListener('click', (event) => {
-    //     modelViewerVariants.variantName = event.target.value === 'default' ? null : event.target.value;
-    // });
+select.addEventListener('click', (event) => {
+        modelViewerVariants.variantName = event.target.value === 'default' ? null : event.target.value;
+    });
     
